a83f6de11a6add5a3c72855caab2925374e45aee
vahkhachatryan <vahekhachatryan000@gmail.com>
2023-04-30 12:00:51 +0400

    fix comment in checkbox component

--- components/molecules/Industry/Industry.js
+++ components/molecules/Industry/Industry.js
@@ -1,23 +1,17 @@
-
 import { memo } from "react";
-import { Row, Col, Paragraph } from "../../atoms";
-import React, { useState } from 'react';
-
-
-
-
+import { Col } from "../../atoms";
 import styles from "./Industry.module.scss";
 import { Checkbox } from "../../atoms";
 
 const Industry = () => {
-  
-     return(
-  <Col className={styles.industryCol}> 
-    <div className={styles.industryMain}>
-         <Checkbox className={styles.checkbox} />
-         <span className={styles.NameText}>Logistic</span>
-    </div>
-  </Col>
-   )
-} 
+  return (
+    <Col className={styles.industryCol}>
+      <div className={styles.industryMain}>
+        <Checkbox className={styles.checkbox} />
+        <span className={styles.NameText}>Logistic</span>
+      </div>
+    </Col>
+  )
+}
+
 export default memo(Industry)
