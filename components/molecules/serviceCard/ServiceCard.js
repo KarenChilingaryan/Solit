8aeb96ac48695b53079ba9fa33dc5d30504fbf1c
KarenChilingaryan <karen.chilingaryann98@gmail.com>
2023-07-17 22:22:26 +0400

    fixed build issue

--- components/molecules/serviceCard/ServiceCard.js
+++ components/molecules/serviceCard/ServiceCard.js
@@ -1,6 +1,7 @@
 import { memo } from "react";
 import Image from "next/image";
 import { Row, Col } from "../../atoms";
+import cardIcon from "../../../assets/img/cardImage.png";
 
 import styles from "./ServiceCard.module.scss";
 
@@ -12,10 +13,10 @@ const ServiceCard = ({ item, onClick, className }) => {
       onClick={onClick}
     >
       <Row align_items={"center"} className={styles.title_iconWrapper}>
-        <Row className={styles.title}>{item.title}</Row>
-        <Image className={styles.iconWrapper} src={item.original_image_service} width={300} height={500}/>
+        <Row className={styles.title}>{item?.title || "Mobile Development"}</Row>
+        <Image className={styles.iconWrapper} src={item?.original_image_service  || cardIcon} width={300} height={500}/>
         <Row className={styles.description}>
-          {item.description}
+          {item?.description || "It is a long established fact that a reader will be distracted by the readable content"}
         </Row>
       </Row>
     </Col>
