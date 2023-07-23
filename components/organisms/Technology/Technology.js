bead75d35fcf7b7119120b4fd5b6e601e89e336a
Sargis333 <101509381+Sargis333@users.noreply.github.com>
2023-07-23 18:08:34 +0400

    technology_filter

--- components/organisms/Technology/Technology.js
+++ components/organisms/Technology/Technology.js
@@ -8,6 +8,7 @@ import { useSelector } from "react-redux";
 
 const Technology = () => {
   const [current, setCurrent] = useState(0);
+  const [filter, setFilter] = useState("back-end");
 
   const postsMainTechnologyApi = useSelector(
     (state) => state?.postsMainTechnologyApi?.queries?.["posts(undefined)"]?.data
@@ -16,26 +17,52 @@ const Technology = () => {
     (state) => state?.postsMainTechnologyFiltersApi?.queries?.["posts(undefined)"]?.data
   );
 
+
+  const filterIcons = () => {
+    if (filter === "front-end") {
+      return postsMainTechnologyApi?.data_list?.filter((el) => el.main_technology_name.toLowerCase() === "react");
+    } else if (filter === "back-end") {
+      return postsMainTechnologyApi?.data_list?.filter((el) => el.main_technology_name.toLowerCase() !== "react");
+    }
+    return [];
+  };
+
+
+
+  const filteredIcons = filterIcons() || [];
+
   return (
     <div className={styles.container}>
-      <Paragraph className={styles.title}>{postsMainTechnologyApi?.data_text ? postsMainTechnologyApi?.data_text[0].title : ""}</Paragraph>
-      <div className={styles.description}
-        dangerouslySetInnerHTML={{ __html: postsMainTechnologyApi?.data_text ? postsMainTechnologyApi?.data_text[0].description : "" }}
+      <Paragraph className={styles.title}>
+        {postsMainTechnologyApi?.data_text ? postsMainTechnologyApi?.data_text[0].title : ""}
+      </Paragraph>
+      <div
+        className={styles.description}
+        dangerouslySetInnerHTML={{
+          __html: postsMainTechnologyApi?.data_text ? postsMainTechnologyApi?.data_text[0].description : "",
+        }}
       />
       <div className={styles.buttonsParent}>
         <div className={styles.buttons}>
-          {postsMainTechnologyFiltersApi && postsMainTechnologyFiltersApi.map((el, i) => (
-            <Button
-              text={el.filter_name_main_technology}
-              lightBlueTech={i === current}
-              grayTextBtnTech={i !== current}
-              key={i}
-            />
-          ))}
+
+          <Button
+            text="Back-end"
+            lightBlueTech={filter === "back-end"}
+            grayTextBtnTech={filter !== "back-end"}
+            onClick={() => setFilter("back-end")}
+          />
+
+
+          <Button
+            text="Front-end"
+            lightBlueTech={filter === "front-end"}
+            grayTextBtnTech={filter !== "front-end"}
+            onClick={() => setFilter("front-end")}
+          />
         </div>
       </div>
       <div className={styles.languages}>
-        {postsMainTechnologyApi?.data_list?.map((el, i) => (
+        {filteredIcons.map((el, i) => (
           <div className={styles.languageBlock} key={i}>
             <Image src={el.technology_logos_for_main.original_logo} className={styles.icon} width={20} height={25} />
             <Paragraph className={styles.name}>{el.main_technology_name}</Paragraph>
