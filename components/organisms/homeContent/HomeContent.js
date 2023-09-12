ce1cb6652de3b0b06aac84d0953775f8456c600d
KarenChilingaryan <karen.chilingaryann98@gmail.com>
2023-09-13 02:01:25 +0400

    fixed

--- components/organisms/homeContent/HomeContent.js
+++ components/organisms/homeContent/HomeContent.js
@@ -363,7 +363,7 @@ const HomeContent = () => {
                 key={i}
                 number={about.rating_number}
                 title={about.rating_text}
-                image={about.webp_icons_impact_we_make}
+                image={about.original_logo_company_of_expert}
                 withOutBG={true}
               />
             ))}
