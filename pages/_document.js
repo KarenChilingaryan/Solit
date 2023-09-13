aaa42b6f1b8552b139c6f633fc47a274bf332b8e
KarenChilingaryan <karen.chilingaryann98@gmail.com>
2023-09-14 00:37:08 +0400

    c

--- pages/_document.js
+++ pages/_document.js
@@ -16,18 +16,18 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <meta name="title" content="Solit" />
-          <meta property="og:description" content="Lorem Ipsum SOLIT" />
+          <meta property="og:description" content="Software development company SolIT" />
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="Solit Website Url" />
           <meta property="twitter:title" content="Solit" />
-          <meta property="twitter:description" content="Lorem Ipsum twit" />
+          <meta property="twitter:description" content="Software development company SolIT" />
           <meta
             name="viewport"
             content="width=device-width, minimum-scale=1.0, maximum-scale = 1.0, user-scalable = no"
           />
           <meta
             property="twitter:image"
-            content="https://simpl.info/webp/cherry.webp"
+            content="https://djnago-solit-static.s3.eu-north-1.amazonaws.com/media/images/converted_image_ZuJcRnF.png"
           />
         </Head>
         <body>
