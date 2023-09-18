59ddfcd2ef054510b7f6c195fe21621abb6a92c3
KarenChilingaryan <karen.chilingaryann98@gmail.com>
2023-09-19 00:55:40 +0400

    fixed inputes

--- components/molecules/floatInput/FloatInput.js
+++ components/molecules/floatInput/FloatInput.js
@@ -58,6 +58,7 @@ const FloatInput = ({
           defaultValue={value}
           showUploadList={showUploadList}
           suffix={suffix}
+          inputMode="text"
           {...rest}
         />
       ) : (
