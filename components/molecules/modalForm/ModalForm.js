df2001a1c28d673ef0e17f80e7d16fa99f7cae5b
KarenChilingaryan <karen.chilingaryann98@gmail.com>
2023-10-18 19:49:34 +0400

    fixed modal values

--- components/molecules/modalForm/ModalForm.js
+++ components/molecules/modalForm/ModalForm.js
@@ -52,84 +52,15 @@ const ModalForm = ({ title, style = {}, data, onSubmit, from = 'apply', classNam
           >
             <FloatInput label="Email" placeholder="Email" name="from_email" />
           </FormItem>
-          {from == 'apply' &&
-            <FormItem
-              name="position"
-              rules={[
-                {
-                  required: true,
-                  message: "Position is required",
-                },
-              ]}
-            >
-              <FloatInput label="Position" placeholder="Position" />
-            </FormItem>}
-          {from == 'apply' &&
-            <FormItem
-              name="level"
-              rules={[
-                {
-                  required: true,
-                  message: "Level is required",
-                },
-              ]}
-            >
-              <FloatInput label="Level" placeholder="Level" />
-            </FormItem>}
-          {from == 'lets' &&
-            <FormItem
-              name="industry"
-              rules={[
-                {
-                  required: true,
-                  message: "Industry is required",
-                },
-              ]}
-            >
-              <FloatInput label="Industry" placeholder="Industry" />
-            </FormItem>}
-          {from == 'lets' &&
-            <FormItem
-              name="service"
-              rules={[
-                {
-                  required: true,
-                  message: "Service is required",
-                },
-              ]}
-            >
-              <FloatInput label="Service" placeholder="Service" />
-            </FormItem>}
-          {from == 'apply' &&
-            <FormItem
-              name="link_to_linkedin"
-              rules={[
-                {
-                  required: true,
-                  message: "Linkedin link is required",
-                },
-              ]}
-            >
-              <FloatInput label="Link to LinkedIn *" placeholder="Link to LinkedIn *" />
-            </FormItem>}
-          {from == 'apply' &&
-            <FormItem
-              name="link_to_github_or_portfolio"
-              rules={[
-                {
-                  required: true,
-                  message: "Github link is required",
-                },
-              ]}
-            >
-              <FloatInput label="Link to Github/Portfolio" placeholder="Link to Github/Portfolio" />
-            </FormItem>}
-
-          <FormItem name={from == 'apply' ? "file_cv" : from == 'lets' ? "upload_document" : "file_document"}>
+          
+          <FormItem name="phon_number" >
+            <FloatInput label="Phone number" placeholder="Phone number" name="phon_number" type="number" />
+          </FormItem>
+          <FormItem name={"upload_document"}>
             <FloatInput
-              label={from == 'apply' ? "Upload your CV" : from == 'lets' ? "Upload document" : "About your project"}
-              placeholder={from == 'apply' ? "Upload your CV" : from == 'lets' ? "Upload document" : "About your project"}
-              name={from == 'apply' ? "file_cv" : from == 'lets' ? "upload_document" : "file_document"}
+              label={"Upload document"}
+              placeholder={"Upload document"}
+              name={"upload_document"}
               multiple
               type="file"
               accept=".pdf,.doc,.docx"
@@ -141,17 +72,11 @@ const ModalForm = ({ title, style = {}, data, onSubmit, from = 'apply', classNam
               className={styles.uploadFile}
             />
           </FormItem>
-          {from != 'apply' && <FormItem name="comment">
+           <FormItem name="comment">
             <FloatInput label="Comment" placeholder="Comment" name="comment" />
-          </FormItem>}
-          {from == 'apply' && <FormItem name="cover_letter">
-            <FloatInput label="Cover letter" placeholder="Cover letter" name="cover_letter" />
-          </FormItem>}
-          {from != 'apply' && <FormItem name="phon_number" >
-            <FloatInput label="Phone number" placeholder="Phone number" name="phon_number" type="number" />
-          </FormItem>}
+          </FormItem>
 
-          {from != 'apply' && <FormItem
+           <FormItem
             name="your_budget"
             rules={[
               {
@@ -173,8 +98,8 @@ const ModalForm = ({ title, style = {}, data, onSubmit, from = 'apply', classNam
             >
               <Select.Option value="demo">Demo</Select.Option>
             </Select>
-          </FormItem>}
-          <FormItem className={styles.accept}>
+          </FormItem>
+          <FormItem className={`${styles.accept} ${secondCheckBox && styles.acceptLeft}`}>
             <Checkbox />
             <Row className={styles.acceptText}>
               I accept your Privacy Policy
