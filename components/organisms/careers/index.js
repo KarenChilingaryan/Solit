872a836465432b5031e011596bb6a8a0520ded32
KarenChilingaryan <karen.chilingaryann98@gmail.com>
2023-11-22 22:03:00 +0400

    d

--- components/organisms/careers/index.js
+++ components/organisms/careers/index.js
@@ -215,7 +215,7 @@ const Careers = () => {
             />
           ))}
         </div>
-        {checked && !isSafari ? <div id="to-jobs"></div> : <></>}
+        {checked ? <div id="to-jobs"></div> : <></>}
         <div className={styles.secondInfo}>
           <div className={styles.secondTitle}>
 
@@ -235,7 +235,8 @@ const Careers = () => {
           />
         </Row>
         <Row className={styles.weKnowSection}>
-          {checked && isSafari ? <div id="to-jobs"></div> : <></>}
+          {/* {checked && isSafari ? <div id="to-jobs"></div> : <></>}
+          <div id="to-jobs">afsf</div> */}
           <WhatToKnow
             title="If you haven't found position..."
             description="For further information don't hesitate to contact us. We would be happy to provide you with more information."
