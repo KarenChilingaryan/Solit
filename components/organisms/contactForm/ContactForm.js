40e3b2e11015349c58dd9299675f1876bf15ae69
KarenChilingaryan <karen.chilingaryann98@gmail.com>
2023-12-12 00:28:55 +0400

    s

--- components/organisms/contactForm/ContactForm.js
+++ components/organisms/contactForm/ContactForm.js
@@ -11,7 +11,7 @@ import upload from "../../../assets/img/uploadIcon.svg";
 import contactBgImage from "../../../assets/img/contact_bg.png";
 import contactUsBgImage from "../../../assets/img/contactus-background.png";
 import close from "../../../assets/img/icons/close.svg";
-import { checkFormValidation } from "../../../utils/hooks/checkRecaptchaValidation";
+// import { checkFormValidation } from "../../../utils/hooks/checkRecaptchaValidation";
 import SuccessModal from "../successModal/SuccessModal";
 
 import styles from "./ContactForm.module.scss";
@@ -26,7 +26,7 @@ const ContactForm = ({
 }) => {
   const [form] = Form.useForm();
   const [file, setFile] = useState();
-  const recaptchaRef = useRef();
+  // const recaptchaRef = useRef();
   const [openSuccess, setOpenSuccess] = useState(false);
   const [onChangeCheckbox, setOnChangeCheckbox] = useState(false);
   const [top, setTop] = useState(0);
@@ -274,7 +274,7 @@ const ContactForm = ({
               },
             ]}
           >
-            <ReCAPTCHA
+            {/* <ReCAPTCHA
               ref={recaptchaRef}
               style={{ width: "300px" }}
               className={styles.recaptcha}
@@ -282,7 +282,7 @@ const ContactForm = ({
                 checkFormValidation(changeRecaptcha, recaptchaRef.current)
               }
               sitekey="6Lee0CIoAAAAAB_dq-qSv6jLMpVn--g2ny42Ww_D"
-            />
+            /> */}
           </FormItem>
 
           <Col className={styles.buttonWrapper}>
