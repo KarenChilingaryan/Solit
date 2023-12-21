25cca3f7d2d638d43191e72318474721e104c79d
KarenChilingaryan <karen.chilingaryann98@gmail.com>
2023-12-22 00:46:01 +0400

    df

--- components/organisms/page-wrapper.js
+++ components/organisms/page-wrapper.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "axios"
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
@@ -46,75 +46,60 @@ const PageWrapper = ({ children, item }) => {
   const dispatch = useDispatch();
 
   const a = useRouter();
-  const getAllData = async (flag = false) => {
-    console.log(a.pathname.length, 'OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO');
+  const getAllData = async (flag = true) => {
     if (a.pathname === "/" || flag) {
-      console.log(">>>>>>>>>>>>>>>>>>", flag, a.pathname === "/");
       await dispatch(await postsApi.endpoints.posts.initiate());
       if (!a.pathname.includes("/services"))
         await dispatch(await servicesApi.endpoints.services.initiate());
-      
-      if (!a.pathname.includes("/what-we-do"))
-        await dispatch(await postsWhatWeDoApi.endpoints.posts.initiate());
-      await dispatch(
-        await postsMainTechnologyFiltersApi.endpoints.posts.initiate()
-      );
-     
-      
-      //to do
-      if (!a.pathname.includes("/about-us"))
-        await dispatch(
-          await abutUsCompanyOfExpertsApi.endpoints.about.initiate()
-        );
-      // if (!a.pathname.includes("/blog")) {
-      //   await dispatch(await postsBlogApi.endpoints.blog.initiate());
-      //   await dispatch(await postsFilterNameBlogApi.endpoints.blog.initiate());
-      // }
-
-      // if (!a.pathname.includes("/portfolio")) {
-      //   await dispatch(await postPortfolioApi.endpoints.posts.initiate());
-      //   await dispatch(
-      //     await portfolioFiltersApi.endpoints.portfolioFilters.initiate()
-      //   );
-      // }
-     
-
-      // if (!a.pathname.includes("/terms-and-conditions")) {
-      //   await dispatch(
-      //     await postsPrivacyPolicyOrConditionTermsApi.endpoints.terms.initiate()
-      //   );
-      // }
-      // if (!a.pathname.includes("/privacy-policy")) {
-      //   await dispatch(
-      //     await postsPrivacyPolicyOrConditionPolicyApi.endpoints.policy.initiate()
-      //   );
-      // }
-    }
-
-    if (a.pathname.includes("/") && a.pathname.length === 1 ) {
       await dispatch(await footerApi.endpoints.footer.initiate());
       await dispatch(
         await postsTextMainAboutUsApi.endpoints.mainAbout.initiate()
       );
       await dispatch(await postsMainProcessTextApi.endpoints.posts.initiate());
-      //main
       await dispatch(await postsMainWhatWeDoTextApi.endpoints.posts.initiate());
-      await dispatch(await postsMainOurProjectsApi.endpoints.posts.initiate());
-
+      if (!a.pathname.includes("/what-we-do"))
+        await dispatch(await postsWhatWeDoApi.endpoints.posts.initiate());
+      await dispatch(
+        await postsMainTechnologyFiltersApi.endpoints.posts.initiate()
+      );
       await dispatch(await postsMainTechnologyApi.endpoints.posts.initiate());
-
- 
+      await dispatch(await postsMainOurProjectsApi.endpoints.posts.initiate());
+      if (!a.pathname.includes("/portfolio")) {
+        await dispatch(await postPortfolioApi.endpoints.posts.initiate());
+        await dispatch(
+          await portfolioFiltersApi.endpoints.portfolioFilters.initiate()
+        );
+      }
+      //to do
+      if (!a.pathname.includes("/about-us"))
+        await dispatch(
+          await abutUsCompanyOfExpertsApi.endpoints.about.initiate()
+        );
+      if (!a.pathname.includes("/blog")) {
+        await dispatch(await postsBlogApi.endpoints.blog.initiate());
+        await dispatch(await postsFilterNameBlogApi.endpoints.blog.initiate());
+      }
       await dispatch(await postTestimonialsApi.endpoints.posts.initiate());
       await dispatch(await postsMainContactsTextApi.endpoints.posts.initiate());
+
+      if (!a.pathname.includes("/terms-and-conditions")) {
+        await dispatch(
+          await postsPrivacyPolicyOrConditionTermsApi.endpoints.terms.initiate()
+        );
+      }
+      if (!a.pathname.includes("/privacy-policy")) {
+        await dispatch(
+          await postsPrivacyPolicyOrConditionPolicyApi.endpoints.policy.initiate()
+        );
+      }
     }
 
-    if (a.pathname.includes("/blog")) {
-      console.log(a.pathname.includes("/blog"), flag);
+    if (a.pathname.includes("/blog") || flag) {
       await dispatch(await postsBlogApi.endpoints.blog.initiate());
       await dispatch(await postsFilterNameBlogApi.endpoints.blog.initiate());
     }
 
-    if (a.pathname.includes("/about-us")) {
+    if (a.pathname.includes("/about-us") || flag) {
       await dispatch(await aboutApi.endpoints.about.initiate());
       await dispatch(await abutUsWhatWeDoApi.endpoints.about.initiate());
       await dispatch(await abutUsImpactApi.endpoints.about.initiate());
@@ -124,7 +109,7 @@ const PageWrapper = ({ children, item }) => {
       );
     }
 
-    if (a.pathname.includes("/careers")) {
+    if (a.pathname.includes("/careers") || flag) {
       await dispatch(await careersJobOpeningApi.endpoints.career.initiate());
       await dispatch(
         await postsTextCareersColourfulApi.endpoints.careersAbout.initiate()
@@ -134,26 +119,25 @@ const PageWrapper = ({ children, item }) => {
       );
     }
 
-    if (a.pathname.includes("/services")) {
-      console.log("eeeeeeeeeeeeeeee");
+    if (a.pathname.includes("/services") || flag) {
       await dispatch(await servicesApi.endpoints.services.initiate());
     }
-    if (a.pathname.includes("/portfolio")) {
+    if (a.pathname.includes("/portfolio") || flag) {
       await dispatch(await postPortfolioApi.endpoints.posts.initiate());
       await dispatch(
         await portfolioFiltersApi.endpoints.portfolioFilters.initiate()
       );
     }
-    if (a.pathname.includes("/what-we-do")) {
+    if (a.pathname.includes("/what-we-do") || flag) {
       await dispatch(await postsWhatWeDoApi.endpoints.posts.initiate());
     }
 
-    if (a.pathname.includes("/terms-and-conditions")) {
+    if (a.pathname.includes("/terms-and-conditions") || flag) {
       await dispatch(
         await postsPrivacyPolicyOrConditionTermsApi.endpoints.terms.initiate()
       );
     }
-    if (a.pathname.includes("/privacy-policy")) {
+    if (a.pathname.includes("/privacy-policy") || flag) {
       await dispatch(
         await postsPrivacyPolicyOrConditionPolicyApi.endpoints.policy.initiate()
       );
@@ -167,7 +151,7 @@ const PageWrapper = ({ children, item }) => {
   useEffect(() => {
     dispatch(headerApi.endpoints.header.initiate());
     getAllData(false);
-  }, [a.pathname]);
+  }, [id]);
 
   return <div>{children}</div>;
 };
