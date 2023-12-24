0c5cb393d543168328cef20b27c4170b6f3d56e8
KarenChilingaryan <karen.chilingaryann98@gmail.com>
2023-12-24 22:05:29 +0400

    s

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-  // ...
+  mode: "production",
   optimization: {
     splitChunks: {
       chunks: "async",
