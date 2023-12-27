a00cce11d8a144e617f309a5f24994aeb212fd08
KarenChilingaryan <karen.chilingaryann98@gmail.com>
2023-12-28 03:43:57 +0400

    z

--- next.config.js
+++ next.config.js
@@ -1,23 +1,11 @@
 /** @type {import('next').NextConfig} */
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
-  openAnalyzer: false,
 });
-
-const pluginOptions = {
-  options: {
-    remarkPlugins: [], // Add Remark plugins for MDX processing
-    rehypePlugins: [], // Add Rehype plugins for MDX processing
-  },
-};
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    webVitalsAttribution: ["CLS", "LCP"],
-    esmExternals: true,
-  },
   compiler: {
     styledComponents: {
       displayName: true,
@@ -27,20 +15,5 @@ module.exports = withBundleAnalyzer({
   },
   images: {
     domains: ["djnago-solit-static.s3.amazonaws.com"],
-  },
-  // Add the PostCSS configuration with PurgeCSS
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.mdx/,
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: "@mdx-js/loader",
-          options: pluginOptions.options,
-        },
-      ],
-    });
-
-    return config;
   },
 });
