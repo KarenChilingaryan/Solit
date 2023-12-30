94891aa816ce90aaaa569af2abc516585d9c4ca5
KarenChilingaryan <karen.chilingaryann98@gmail.com>
2023-12-31 00:13:07 +0400

    zz

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
 
-module.exports = withBundleAnalyzer({
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
@@ -16,4 +13,4 @@ module.exports = withBundleAnalyzer({
   images: {
     domains: ["djnago-solit-static.s3.amazonaws.com"],
   },
-});
+};
